9856f4851b5c55ca29f9bc20875251c929b137bd
JOAOPEDRODINIZDEV <115243882+JOAOPEDRODINIZDEV@users.noreply.github.com>
2022-12-17 11:50:43 -0300

    Add files via upload

---
+++ Conversor.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+float Celsius_Fahrenheit(float tc){
+  float r;
+    r=(tc*1.8)+32;
+
+return r;
+}
+
+
+  float Fahrenheit_Celsius(float tf){
+    float r;
+  
+    r= (tf-32)/1.8;
+
+  return r;
+
+
+}
+float Celsius_Kelvin(float tc){
+ float r;
+  r= tc+273.15;
+ 
+return r;
+}
+
+int main(){
+int op, op1;
+	  float tc, tf,tk, r;
+ 
+ printf( " Conversor de temperatura");
+ printf("Escolhar um opção");
+ printf("\n1- Celcius Para fahrenheit\n");
+ printf("2- Fahrenheit Para Celsius\n");
+ printf("3-celsius Para Kelvin\n");
+ scanf("%d",&op);
+ printf("Digite a temperatura\n");
+
+ if(op==1){
+printf("Em Calsius:");
+scanf("%f",&tc);
+tf=Celsius_Fahrenheit(tc);
+
+}else if(op==2){
+   printf("Em Fahrenheit:");
+    scanf("%f",&tf);
+    
+    
+    tc= Fahrenheit_Celsius(tf);
+}else if (op==3){
+   printf("Em kelvin:");
+scanf("%f",&tk);
+tk= Celsius_Kelvin(tc);
+ 
+}
+printf("Temperatura em Celsius é : %.2f\n",tc);
+printf("Temperatura em fahrenheit é : %.2f\n",tf);
+printf("Temperatura em Kelvin é : %.2f\n",tk);
+ 
+
+return 0;
+}
+
+
+
+    
+    
+    
+  
+ 
+    
